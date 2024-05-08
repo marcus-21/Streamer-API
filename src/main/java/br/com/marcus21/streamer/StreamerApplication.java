32de1fb7a4a36e5d5033155445e4b6410cb4bd4f
marksv21 <marvmms@gmail.com>
2024-05-08 01:33:35 -0300

    refactor: classe raiz

--- src/main/java/br/com/marcus21/streamer/StreamerApplication.java
+++ src/main/java/br/com/marcus21/streamer/StreamerApplication.java
@@ -1,11 +1,17 @@
 package br.com.marcus21.streamer;
 
+import br.com.marcus21.streamer.models.DadosEpisodio;
 import br.com.marcus21.streamer.models.DadosSerie;
+import br.com.marcus21.streamer.models.DadosTemporada;
+import br.com.marcus21.streamer.principal.Principal;
 import br.com.marcus21.streamer.services.ConsumoAPI;
 import br.com.marcus21.streamer.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class StreamerApplication implements CommandLineRunner {
@@ -16,12 +22,8 @@ public class StreamerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibirMenu();
 
 	}
 }
