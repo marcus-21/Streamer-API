dd23417e5a4d815a1a0337fa87d61e8c5435179c
marksv21 <marvmms@gmail.com>
2024-05-16 01:59:52 -0300

    finalizando o projeto

---
+++ src/main/java/br/com/marcus21/streamer/models/Episodio.java
@@ -0,0 +1,80 @@
+package br.com.marcus21.streamer.models;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private String avaliacao;
+    private String dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numero();
+        try {
+            this.avaliacao = String.valueOf(Double.valueOf(dadosEpisodio.avaliacao()));
+        } catch (NumberFormatException ex) {
+            this.avaliacao = String.valueOf(0.0);
+        }
+
+        try {
+            this.dataLancamento = String.valueOf(LocalDate.parse(dadosEpisodio.dataLancamento()));
+        } catch (DateTimeParseException ex) {
+            this.dataLancamento = null;
+        }
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public String getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(String avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao='" + avaliacao + '\'' +
+                ", dataLancamento='" + dataLancamento;
+    }
+}
--- src/main/java/br/com/marcus21/streamer/principal/Principal.java
+++ src/main/java/br/com/marcus21/streamer/principal/Principal.java
@@ -3,12 +3,14 @@ package br.com.marcus21.streamer.principal;
 import br.com.marcus21.streamer.models.DadosEpisodio;
 import br.com.marcus21.streamer.models.DadosSerie;
 import br.com.marcus21.streamer.models.DadosTemporada;
+import br.com.marcus21.streamer.models.Episodio;
 import br.com.marcus21.streamer.services.ConsumoAPI;
 import br.com.marcus21.streamer.services.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -39,6 +41,80 @@ public class Principal {
 //                System.out.println(episodiosTemporada.get(j).titulo());
 //            }
 //        }
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+//        System.out.println("\n Top 10 episódios");
+//        dadosEpisodios.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("primeiro filtro(N/A)" + e))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .peek(e -> System.out.println("Ordenação" + e))
+//                .limit(10)
+//                .peek(e -> System.out.println("Limitação" + e))
+//                .map(e -> e.titulo().toUpperCase())
+//                .peek(e -> System.out.println("Mapeamento" + e))
+//                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+        episodios.forEach(System.out::println);
+
+//        System.out.println("Digite um trecho do título do episódio");
+//        var trechoTitulo = leitura.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//        if(episodioBuscado.isPresent()){
+//            System.out.println("Episódio encontrado!");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
+
+
+//        System.out.println("A partir de que ano você deseja ver os episódios? ");
+//        var ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//
+//        episodios.stream()
+//                .filter(e -> e.getDataLancamento() != null &&
+//                        LocalDate.parse(e.getDataLancamento(), formatador ).isAfter(dataBusca))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getTemporada() +
+//                                " Episódio: " + e.getTitulo() +
+//                                " Data lançamento: " + LocalDate.parse(e.getDataLancamento(), formatador ).format(formatador )
+//                ));
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> {
+                    try {
+                        return Double.parseDouble(e.getAvaliacao()) > 0.0;
+                    } catch (NumberFormatException ex) {
+                        return false;
+                    }
+                })
+                .collect(Collectors.summarizingDouble(e -> Double.parseDouble(e.getAvaliacao())));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
+
+
+
+
+
+
+
+
+
     }
 }
